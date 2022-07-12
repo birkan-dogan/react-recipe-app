683f5f482710421b5b41a7997ec5a49e883b231a
birkan <b.561birkan@gmail.com>
2022-07-12 20:06:18 +0300

    Login component completed

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,37 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Outlet } from "react-router-dom";
+import image from "../../assets/meal2.svg";
+import styling from "./Login.module.css";
 const Login = () => {
-  return <div>Login</div>;
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  const handleClick = () => {
+    setIsAuthenticated(true);
+  };
+  if (isAuthenticated) {
+    return <Outlet />;
+  }
+  return (
+    <div className={styling.container}>
+      <div>
+        <img src={image} alt="login" className={styling.img} />
+        <h1>`B1rkan` Recipe</h1>
+        <form className={styling.form} onSubmit={handleSubmit}>
+          <input type="text" placeholder="UserName" className={styling.input} />
+          <input
+            type="password"
+            placeholder="PassWord"
+            className={styling.input}
+          />
+          <button className={styling.btn} onClick={handleClick}>
+            LogIN
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../pages/home/Home";
+import Login from "../pages/login/Login";
 
 const AppRouter = () => {
-  return <div>AppRouter</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />}>
+          <Route path="" element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default AppRouter;
