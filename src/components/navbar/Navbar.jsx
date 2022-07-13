1791553b2a7efda7904c2c4e84bf8bf8d89728b6
birkan <b.561birkan@gmail.com>
2022-07-13 12:38:18 +0300

    navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,32 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import styling from "./Navbar.module.css";
+import image from "../../assets/wedding-dinner.png";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <nav className={styling.container}>
+        <Link to="/">
+          <img src={image} alt="food" style={{ width: "75px" }} />
+        </Link>
+        <div className={styling.inner}>
+          <Link to="/about" className={styling.link}>
+            About
+          </Link>
+          <a
+            href="https://github.com/birkan-dogan"
+            target="_blank"
+            className={styling.link}
+          >
+            GitHub
+          </a>
+          <Link to="/" className={styling.link}>
+            LogOut
+          </Link>
+        </div>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
